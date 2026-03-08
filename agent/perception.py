77e04512e60046f52538df50bcb1dfa6f7be8d23
Fran <franciscosoler.dev@gmail.com>
2026-03-08 23:22:29 +0100

    Remove distance_label from perception.py
    - Unused after parse_nearby removal

--- agent/perception.py
+++ agent/perception.py
@@ -2,16 +2,6 @@ import math
 from config import TOUCH_DISTANCE
 
 
-def distance_label(distance):
-    if distance < 200:
-        return "very close"
-    elif distance < 500:
-        return "nearby"
-    elif distance < 1000:
-        return "moderate distance"
-    else:
-        return "far away"
-
 def get_touching_from_spatial(agent_x, agent_y, spatial):
     for obj_name, data in spatial.items():
         dx = data["x"] - agent_x
