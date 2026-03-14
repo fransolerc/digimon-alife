d3c627fcb1f8cb5e7fac302f64e83798ff6f3a63
Fran <franciscosoler.dev@gmail.com>
2026-03-14 22:01:13 +0100

    fix(debug): remove unused debug_options route for cleaner code

--- main.py
+++ main.py
@@ -45,13 +45,6 @@ def debug_agent(agent_id):
     return _cors(make_response(jsonify(data), 200))
 
 
-@app.route('/debug/<agent_id>', methods=['OPTIONS'])
-def debug_options(_agent_id):
-    resp = make_response("", 204)
-    resp.headers["Access-Control-Allow-Origin"] = "*"
-    resp.headers["Access-Control-Allow-Methods"] = "GET, OPTIONS"
-    return resp
-
 def _get_or_create_agent(agent_id):
     if agent_id not in agents:
         name = agent_id.split("_")[0].capitalize()
